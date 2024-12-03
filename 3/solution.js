cb5ab8f43f919f2f22ad7c7818ca5136e92aa733
Csaba Tibor Padar <csabatibor.padar@hu.fressnapf.eu>
2024-12-03 10:04:02 +0100

    DAY 3: Add implementation part II

--- 3/solution.js
+++ 3/solution.js
@@ -1,17 +1,30 @@
 import { readFile } from 'fs/promises';
 
-const REGEX_PATTERN = /mul\(\d+,\d+\)/g; // Provided by ChatGPT, NGL :-)
+// Provided by ChatGPT, NGL :-)
+const REGEX_PATTERN = /mul\(\d+,\d+\)|don't\(\)|do\(\)/g;
+
+const COMMAND_ENABLE = `do()`;
+const COMMAND_DISABLE = `don't()`;
 
 const input = await readFile('input.txt', 'utf8');
 
-const mulInstructions = input.match(REGEX_PATTERN);
-const mulParameters = mulInstructions.map((ins) => ins.substring(4, ins.length - 1));
+const instructions = input.match(REGEX_PATTERN);
 
 let total = 0;
+let shouldMultiply = true;
 
-mulParameters.forEach((parameter) => {
-  const [x, y] = parameter.split(',').map(Number);
-  total += x * y;
+instructions.forEach((instruction) => {
+  if ([COMMAND_ENABLE, COMMAND_DISABLE].includes(instruction)) {
+    shouldMultiply = instruction === COMMAND_ENABLE;
+    return;
+  }
+
+  // multiply instruction
+  if (shouldMultiply) {
+    const parameters = instruction.substring(4, instruction.length - 1);
+    const [x, y] = parameters.split(',').map(Number);
+    total += x * y;
+  }
 });
 
 console.log(`Result of the multiplications: ${total}`);
